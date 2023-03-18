dbf0efe539a0c71d831cafacc7017a30d14fb70b
Yakov Kolyada <yakov.kolyada@gmail.com>
2023-03-18 21:50:54 +0200

    main

---
+++ src/main/java/com/example/HelloJavaApp.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello Java!
+ *
+ */
+public class HelloJavaApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Java!" );
+    }
+}
